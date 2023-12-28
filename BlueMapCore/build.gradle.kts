e81bcee917b84ab224459053e11ac22240b7204a
Lukas Rieger (Blue) <TBlueF@users.noreply.github.com>
2023-12-28 11:30:22 +0100

    Update brotli lib version

--- BlueMapCore/build.gradle.kts
+++ BlueMapCore/build.gradle.kts
@@ -65,11 +65,11 @@ dependencies {
     api ("org.apache.commons:commons-dbcp2:2.9.0")
     api ("io.airlift:aircompressor:0.24")
 
-    api (group = "com.aayushatharva.brotli4j", name = "brotli4j", version = "1.4.2")
-    api (group = "com.aayushatharva.brotli4j", name = "native-windows-x86_64", version = "1.4.2")
-    api (group = "com.aayushatharva.brotli4j", name = "native-linux-aarch64", version = "1.4.2")
-    api (group = "com.aayushatharva.brotli4j", name = "native-osx-x86_64", version = "1.4.2")
-    api (group = "com.aayushatharva.brotli4j", name = "native-linux-x86_64", version = "1.4.2")
+    api (group = "com.aayushatharva.brotli4j", name = "brotli4j", version = "1.14.0")
+    api (group = "com.aayushatharva.brotli4j", name = "native-windows-x86_64", version = "1.14.0")
+    api (group = "com.aayushatharva.brotli4j", name = "native-linux-aarch64", version = "1.14.0")
+    api (group = "com.aayushatharva.brotli4j", name = "native-osx-x86_64", version = "1.14.0")
+    api (group = "com.aayushatharva.brotli4j", name = "native-linux-x86_64", version = "1.14.0")
 
     api ("de.bluecolored.bluemap.api:BlueMapAPI")
 
